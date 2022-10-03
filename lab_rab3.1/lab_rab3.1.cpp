68a9bc973dbeab3df15928ae310f37c938cf5f79
Filipp_Timos <filipptim000@gmail.com>
2022-10-03 21:14:57 +0300

    first commit

---
+++ lab_rab3.1/lab_rab3.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int var1 = 100;
+    int* var0;
+    var0 = &var1;
+    cout << "Адресс перемнной - "<< var0 << endl;
+    cout << "Значение переменной = " << *var0 << endl;
+    *var0 = 1;
+    cout << "Измененное значение переменной = " << var1 << endl;
+
+
+    int A[3] = { 1, 2, 3 };
+    cout << "{ "<< *A;
+    for (int i = 1; i <= 2; i++)
+    {
+        cout << ", " << *(A + i);
+    }
+    cout << " }"<<endl;
+
+    int var2 = 5;
+    int* const var02 = &var2;
+    cout << "Значение переменной до изменения = " << var2 << endl;
+    *var02 = 111;
+    cout<< "Значение переменной после изменения = " << var2 << endl;
+
+}
+
+
---
+++ lab_rab3.2/lab_rab3.2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int func_add(int x, int y)
+{
+    return (x + y);
+}
+int func_sub(int x, int y)
+{
+    return (x - y);
+}
+
+
+int add(int *start, int *stop)
+{
+    int summ = 0;
+    for (int* tmp = start; tmp != stop; tmp++)
+    {
+        summ += *tmp;
+    }
+    return summ;
+}
+
+int func_sym(char a, int x, int y)
+{
+    if (a == '+')
+        return func_add(x, y);
+    if (a == '-')
+        return func_sub(x, y);
+}
+    
+    
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    /*Создайте функцию, которая принимает 2 указателя(на начало и конец массива) 
+    и которая возвращает сумму всех элементов массива.*/
+    int A[] = { 1, 2, 3, 9, 8, 7 };
+    int* begin = std::begin(A);
+    int* end = std::end(A);
+    cout << "Сумма элементов массива равна = ";
+    cout << add(begin, end) << endl<<endl;
+
+    cout<<func_sym( '+', 100, 10);
+
+
+    float* var1 = new float (12.5f);
+    cout << endl << *var1;
+    delete var1;
+
+}
